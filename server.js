e30f6d79fcb37d4ed67081bcff86c1a059232850
leowang <leowang9863@gmail.com>
2021-11-21 22:54:46 -0500

    hello world

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const app = express();
+
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shrink", async (req, res) => {
+  await ShortUrl.create({ originalUrl: req.body.fullUrl });
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ shortUrl: req.params.shortUrl });
+  if (shortUrl == null) {
+    res.send("No such url found");
+  }
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.originalUrl);
+});
+
+app.listen(process.env.PORT || 5000);
